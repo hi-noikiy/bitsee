0e7ed241b8b27445ebed5e76bf572655ca09a17b
king729 <87287864@qq.com>
2018-02-09 12:02:05 +0800

    debug

---
+++ plugins/king/market/updates/create_ticker_data.php
@@ -0,0 +1,33 @@
+<?php namespace King\Market\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class CreateTickerData extends Migration
+{
+
+    public function up()
+    {
+        Schema::create('tickers', function($table)
+        {
+            $table->engine = 'InnoDB';
+            $table->unsignedBigInteger('id')->primary();
+            $table->double('high')->nullable();
+            $table->double('low')->nullable();
+            $table->double('vol')->nullable();
+            $table->double('open')->nullable();
+            $table->double('close')->nullable();
+            $table->enum('symbol', ['BTC/USDT', 'LTC/BTC']);
+            $table->enum('market', ['huobipro', 'cokex']);
+            $table->dateTime('date_at')->nullable();
+            $table->timestamp('published_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::drop('tickers');
+    }
+
+}
