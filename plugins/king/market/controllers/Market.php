aa76409b3b4543d9c14a71a21817f4f3e4e31ab3
king729 <87287864@qq.com>
2018-02-21 17:06:36 +0800

    debug

--- plugins/king/market/controllers/Market.php
+++ plugins/king/market/controllers/Market.php
@@ -69,6 +69,8 @@ class Market extends Controller
                 $symbols[] =  $symbol;
             }
 
+            $model->symbols()->create($symbols);
+
             return $symbols;
 
             
