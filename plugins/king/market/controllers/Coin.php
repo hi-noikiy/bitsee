4f7ee9521078e2803121e074641821c093585451
king729 <87287864@qq.com>
2018-04-25 17:11:37 +0800

    debug

--- plugins/king/market/controllers/Coin.php
+++ plugins/king/market/controllers/Coin.php
@@ -10,6 +10,9 @@ use DB;
 
 use King\Market\Models\Coin as CoinModel;
 
+use King\Market\Models\Member;
+
+
 use Vdomah\Excel\Classes\Excel;
 
 class Coin extends Controller
@@ -152,6 +155,122 @@ class Coin extends Controller
 
     }
 
+    public function onUpdateMemberIcon()
+    {
+        $members = Member::with('icon')->get();
+        $numadd = 0;
+        $numupdate = 0;
+        $numfilenotexist = 0;
+        $numerror = 0;
+        foreach($members as $member) {
+
+            $file = new File();
+
+            try{
+                $file = $file->fromFile('./storage/app/media/members/'.$member->avatar);
+            }catch(\Exception $ex){
+                $numfilenotexist++;
+                continue;
+            }
+            if($file) {
+
+                $flag = 0;
+                DB::beginTransaction();
+                try{
+                    if($member->icon) {
+
+                        $member->icon->delete();
+                        $flag = 1;
+
+                    }
+
+                    $file->attachment_type = 'King\Market\Models\Member';
+                    $file->attachment_id = $member->id;
+                    $file->field = 'icon';
+                    $file->is_public = 1;
+                    $file->save();
+                    DB::commit();
+                    if($flag) {
+                        $numupdate++;
+                    }else{
+                        $numadd++;
+                    }
+                }catch(\Exception $ex){
+                    $numerror++;
+                    DB::rollback();
+                }
+
+            
+            }
+
+            
+        }
+        if($numerror){
+            Flash::error('insert:  '.$numadd.'  update: '.$numupdate.'  error:  '.$numerror.'  notexist '.$numfilenotexist);
+        }else{
+            Flash::success('insert:  '.$numadd.'  update: '.$numupdate.'  error:  '.$numerror.'  notexist '.$numfilenotexist);
+        }
+    }
+
+    public function onUpdateMembers()
+    {
+        Excel::excel()->load(base_path() . '/storage/app/media/members.xlsx', function($reader) {
+
+            $results = $reader->all()->toArray();
+            $updatenum = 0;
+            $insertnum = 0;
+            $errornum = 0;
+            foreach ($results as $row => $data) {
+                try {
+    
+                    if (!$title = array_get($data, 'name')) {
+                        Flash::error($row.'  Missing name');
+                        continue;
+                    }
+    
+                    /*
+                     * Find or create
+                     */
+                    $member = Member::make();
+    
+                    //if ($this->update_existing) {
+                    $member = $this->findDuplicateMember($data) ?: $member;
+                    //}
+    
+                    $memberExists = $member->exists;
+    
+                    /*
+                     * Set attributes
+                     */
+                    $except = ['id','ID'];
+    
+                    foreach (array_except($data, $except) as $attribute => $value) {
+                        $member->{$attribute} = $value ?: null;
+                    }
+    
+                    $member->save();//forceSave();
+    
+                    /*
+                     * Log results
+                     */
+                    if ($memberExists) {
+                        $updatenum++;
+                    }
+                    else {
+                        $insertnum++;
+                    }
+                }
+                catch (Exception $ex) {
+                    Flash::error($row.':   '.$ex->getMessage());
+                    $errornum++;
+                }
+            }
+            Flash::success('insert:  '.$insertnum.'update: '.$updatenum,'error:  '.$errornum);
+        });
+
+
+    }
+
     protected function findDuplicateCoin($data)
     {
         // if ($id = array_get($data, 'id')) {
@@ -162,6 +281,19 @@ class Coin extends Controller
         $coin = CoinModel::where('base', $base);
 
         return $coin->first();
+    }
+
+    protected function findDuplicateMember($data)
+    {
+        // if ($id = array_get($data, 'id')) {
+        //     return CoinModel::find($id);
+        // }
+        // 忽略 id 以 base 为准
+        $name = array_get($data, 'name');
+        $base = array_get($data, 'base');
+        $member = Member::where('base', $base)->where('name',$name)->first();
+
+        return $member->first();
     }
 
 }
